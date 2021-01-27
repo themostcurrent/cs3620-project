bccdf46f2e87b5e42ba28fc40695bb789e6cda7d
Joey Sanchez <joeysanchez@mail.weber.edu>
2021-01-27 10:44:01 -0700

    creeated shows

---
+++ shows.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "bookscs3620.mysql.database.azure.com";
+$username = "Joey@bookscs3620";
+$password = "Metobe3!";
+$dbname = "testdb";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO shows (idshows, title)
+VALUES (1, '深夜食堂')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>

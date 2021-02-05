342042487b9f23e5ede5d9eec522f6f58332b94f
Joey Sanchez <joeysanchez@mail.weber.edu>
2021-02-04 17:07:06 -0700

    sbsrb

--- books.php
+++ books.php
@@ -1,4 +1,5 @@
-<?phphead("Access-Control-Allow-Origin: *");
+<?php 
+    header("Access-Control-Allow-Origin: *");
 ?>
 
 {"books-read":[{"Title":"Outliers","Author":"Malcolm Gladwell"},{"Title":"The Talent Code", "Author":"Daniel Coyle"},{"Title":"The Last Wish", "Author":"Andrezej Sapkowski"},{"Title":"The Body", "Author":"Bill Bryson"}]}

e2f8244e591774a97a5b0deaff6ed6f28fd41070
Joey Sanchez <joeysanchez@mail.weber.edu>
2021-02-06 19:26:52 -0700

    current version

--- user/userDAO.php
+++ user/userDAO.php
@@ -1,11 +1,23 @@
+  
 <?php
+class UserDAO {
+  function getUser($user){
+    require_once('./utilities/connection.php');
+    
+    $sql = "SELECT first_name, last_name, username, user_id FROM user WHERE user_id =" . $user->getUserId();
+    $result = $conn->query($sql);
 
-class UserDAO{
-
-    public function getUser($user)
-    {
-        $user->setUsername('Joey');
+    if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $user->setFirstName($row["first_name"]);
+        $user->setLastName($row["last_name"]);
+        $user->setUsername($row["username"]);
     }
+    } else {
+        echo "0 results";
+    }
+    $conn->close();
+  }
 }
-
 ?>
